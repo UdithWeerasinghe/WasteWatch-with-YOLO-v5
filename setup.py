223521a18274d272c77bc9b97d1579691d6ef212
Udith Weerasinghe <118172498+UdithWeerasinghe@users.noreply.github.com>
2025-02-20 13:33:43 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Udith Weerasinghe',
+    author_email= '1999udithweerasinghe@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
