6210ce23c88a98cf0c0d5ae540e93ea90cee36cc
Udith Weerasinghe <118172498+UdithWeerasinghe@users.noreply.github.com>
2025-02-22 02:33:34 +0530

    Model Trainer Stage completed

--- wasteDetection/entity/artifacts_entity.py
+++ wasteDetection/entity/artifacts_entity.py
@@ -13,6 +13,6 @@ class DataValidationArtifact:
 
 
 
-# @dataclass
-# class ModelTrainerArtifact:
-#     trained_model_file_path: str
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
